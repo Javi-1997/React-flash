8cf966eeb73fe82e07d941e0bd036c4224c78fc0
Javier <javii_lb@hotmail.com>
2023-09-20 21:05:57 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tienda Flash
-        </p>
+        <h1>
+          Tienda Flash a
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
