eb17ce57a67f139cf85e9fe926b72cf28787b3d2
Javier <javii_lb@hotmail.com>
2023-10-09 22:11:59 -0300

    routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/NavBar';
 import ItemListContainer from './components/ItemListCointainer';
-
-
-
+import { CartContext } from './context/cartcontext';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
 
 function App() {
   return (
+     <><BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contacto" element={<Contacto />} />
+          <Route path="*" elemento={< />} />
+        </Route>
+      </Routes>
+    </BrowserRouter></>
     <><Navbar></Navbar>
     <ItemListContainer></ItemListContainer>
     <div className="App">
+      <CartContext.Provider value={1}></CartContext.Provider>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
@@ -27,6 +38,7 @@ function App() {
         </a>
       </header>
     </div></>
+    
   );
 }
 
