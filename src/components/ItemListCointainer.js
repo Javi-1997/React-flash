acba10e79bfa8ecf24b43994962b7956006fb96e
Javier <javii_lb@hotmail.com>
2023-11-13 21:31:48 -0300

    firebase

--- src/components/ItemListCointainer.js
+++ src/components/ItemListCointainer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "../FireBase/configuracion";
+import { db } from "../Firebase/config";
 
 
 const ItemListContainer = () => {
